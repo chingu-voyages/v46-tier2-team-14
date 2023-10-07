9f9dd1e2323ed47d904cd19d32cfb45a361e8cdd
diaasaur <diaasaur@gmail.com>
2023-10-07 21:40:18 +0530

    config: add prettier

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,5 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': 'eslint --ignore-path .gitignore --cache --fix',
-  '*.{ts,tsx}': () => 'npm run check:types',
-}
+  "*.{js,jsx,ts,tsx}": "eslint --ignore-path .gitignore --cache --fix",
+  "*.{ts,tsx}": () => "npm run check:types",
+  "*.{jsx,ts,tsx,css,scss}": "prettier --ignore-path .gitignore --write",
+};
