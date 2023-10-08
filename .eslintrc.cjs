2b167b0e9e83c4c65cec5e3e8867d4e4fcb0aa1b
Dikshant Agarwal <dikshantagarwal234@gmail.com>
2023-10-08 18:06:40 +0530

    chore:Added custom eslint rules and update gitignore file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,23 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh',"simple-import-sort","jsx-a11y"],
   rules: {
+    "simple-import-sort/imports": "error",// will sort the import 
+    "simple-import-sort/exports": "error",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "arrow-body-style": ["error", "as-needed"],//This rule can enforce or disallow the use of braces around arrow function body.
+    "react/react-in-jsx-scope":["off"],
+    "react/no-array-index-key": "error",//error  if an element uses an Array index in its key.
+    "react-hooks/exhaustive-deps": "warn",
+    "react-hooks/rules-of-hooks": "error",//enforce rules of hooks
   },
 }
