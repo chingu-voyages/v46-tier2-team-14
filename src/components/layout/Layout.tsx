4799e94d6e4f4ed3f9fd1303a75458e8410ac63b
diaasaur <diaasaur@gmail.com>
2023-10-11 12:54:53 +0530

    add header to layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,9 @@
+import Header from "./header/Header";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Header />
       {children}
       {/* <footer></footer> do we need a footer? */}
     </>
