882150b35f8a8309e00c838616189524dbd16822
sumitgangwar58 <sumitgangwar58@gmail.com>
2023-10-13 20:26:45 +0530

    fix loading  animation and add type

--- src/components/skeleton/Skeleton.tsx
+++ src/components/skeleton/Skeleton.tsx
@@ -1,15 +1,16 @@
 import styles from "./Skeleton.module.css";
 
 interface SkeletonI {
-  width: number;
-  height: number;
-  type: "circle" | "rectangle" | "rounded";
+  width?: number;
+  height?: number;
+  type?: "circle" | "rectangle" | "rounded";
 }
 
 export default function Skeleton({ width, height, type }: SkeletonI) {
   return (
     <div
-      className={`${styles.skeleton} ${styles[type]}`}
+      className={`${styles.skeleton} ${type ? styles[type] : ""} `}
+      aria-busy
       style={{
         width: `${width}rem`,
         height: `${height}rem`,
@@ -17,3 +18,9 @@ export default function Skeleton({ width, height, type }: SkeletonI) {
     />
   );
 }
+
+Skeleton.defaultProps = {
+  width: 10,
+  height: 10,
+  type: "rounded",
+};
