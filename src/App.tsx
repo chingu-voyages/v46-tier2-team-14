16c840e7ca3aef7d51f9add12c9bcad91e547744
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2023-10-14 21:29:19 +0530

    added the route for recepie page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Layout from "./components/layout/Layout";
 import Home from "./pages/home/Home";
+import DetailRecepie from "./pages/home/recepie";
 
 export default function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/recepie" element={<DetailRecepie />} />
       </Routes>
     </Layout>
   );
---
+++ src/pages/home/recepie.tsx
@@ -0,0 +1,4 @@
+function DisplayRecepie() {
+  return <div>This is the recepie page</div>;
+}
+export default DisplayRecepie;
