5fbee6416b236ca155a926df42cda9b6b09474f8
diaasaur <diaasaur@gmail.com>
2023-10-20 15:45:40 +0530

    add RecipeList to Home for testing with dummy data

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,5 @@
+import RecipeList from "../../components/recipe-list/RecipeList";
+import data from "../../util/data.json";
 import Hero from "./hero/Hero";
 import styles from "./home.module.css";
 
@@ -5,6 +7,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Hero />
+      <RecipeList recipes={data} query="Cheese" />
       {/* TODO: component for under 30mins recipes here */}
     </main>
   );
