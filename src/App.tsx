ff950486524f169135bb97372affbafce41a4a49
diaasaur <diaasaur@gmail.com>
2023-10-21 11:24:47 +0530

    removing v3 react-query from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
-import { QueryClient, QueryClientProvider } from "react-query";
 import { Route, Routes } from "react-router-dom";
 
 import Layout from "./components/layout/Layout";
 import Home from "./pages/home/Home";
 import DisplayPost from "./pages/Recipe";
 
-const queryClient = new QueryClient();
-
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-
-          <Route path="/recipe/:postId" element={<DisplayPost />} />
-        </Routes>
-      </Layout>
-    </QueryClientProvider>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipe/:postId" element={<DisplayPost />} />
+      </Routes>
+    </Layout>
   );
 }
