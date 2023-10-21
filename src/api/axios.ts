b0c6b95626506cdfc27d8bfd7ee66bc98c1cfed2
diaasaur <diaasaur@gmail.com>
2023-10-21 15:05:32 +0530

    update env access with VITE prefix

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,10 +1,10 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  baseURL: import.meta.env.BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
-    "X-RapidAPI-Key": import.meta.env.API_KEY,
-    "X-RapidAPI-Host": import.meta.env.API_HOST,
+    "X-RapidAPI-Key": import.meta.env.VITE_API_KEY,
+    "X-RapidAPI-Host": import.meta.env.VITE_API_HOST,
   },
 });
 
