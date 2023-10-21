0df4eddff873fc8b3985d6375c5ea7111ab0de54
diaasaur <diaasaur@gmail.com>
2023-10-21 15:10:42 +0530

    update routes with new recipe page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import { Route, Routes } from "react-router-dom";
 
 import Layout from "./components/layout/Layout";
 import Home from "./pages/home/Home";
-import DisplayPost from "./pages/Recipe";
+import Recipe from "./pages/recipe/Recipe";
 
 export default function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/recipe/:postId" element={<DisplayPost />} />
+        <Route path="/recipe/:recipeId" element={<Recipe />} />
+        <Route path="*" element={<h1>404</h1>} />
       </Routes>
     </Layout>
   );
