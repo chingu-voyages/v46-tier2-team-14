81790a3fbc8757ce966e6fea7a7f69203c4bc22c
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2023-10-25 16:44:28 +0530

    adding nutrition g

---
+++ src/components/nutrition/Nutrition.tsx
@@ -0,0 +1,53 @@
+function NutritionInfo() {
+  return (
+    <div>
+      {/* <h3>Nutrition</h3>
+      <p>Calories: {nutrition.calories}</p>
+      <p>Carbohydrates: {nutrition.carbohydrates}</p>
+      <p>Fat: {nutrition.fat}</p>
+      <p>Protein: {nutrition.protein}</p>
+      <p>Sugar: {nutrition.sugar}</p>
+      <p>Fiber: {nutrition.fiber}</p> */}
+      <h3>Nutrition</h3>
+      <p>Calories: 50</p>
+      <p>Carbohydrates: 1</p>
+      <p>Fat: 4</p>
+      <p>Protein: 7</p>
+      <p>Sugar:8</p>
+      <p>Fiber:6</p>
+    </div>
+  );
+}
+
+export default NutritionInfo;
+
+// import React from "react";
+
+// interface Nutrition {
+//   calories: number;
+//   carbohydrates: number;
+//   fat: number;
+//   protein: number;
+//   sugar: number;
+//   fiber: number;
+// }
+
+// interface NutritionInfoProps {
+//   nutrition: Nutrition;
+// }
+
+// const NutritionInfo: React.FC<NutritionInfoProps> = ({ nutrition }) => {
+//   return (
+//     <div>
+//       <h3>Nutrition</h3>
+//       <p>Calories: {nutrition.calories}</p>
+//       <p>Carbohydrates: {nutrition.carbohydrates}</p>
+//       <p>Fat: {nutrition.fat}</p>
+//       <p>Protein: {nutrition.protein}</p>
+//       <p>Sugar: {nutrition.sugar}</p>
+//       <p>Fiber: {nutrition.fiber}</p>
+//     </div>
+//   );
+// };
+
+// export default NutritionInfo;
