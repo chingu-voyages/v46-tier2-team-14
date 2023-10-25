0c956838f5b2b20362dfccfb401d5e72c7ecfa23
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2023-10-25 17:08:04 +0530

    added the instruction

---
+++ src/components/instruction/Instruction.tsx
@@ -0,0 +1,13 @@
+function Instruction() {
+  return (
+    <div>
+      <h3>Instruction</h3>
+      <ul>
+        <li>Put the flame on </li>
+        <li>Heat the water for 10 min </li>
+        <li>put patotoes in it carefully </li>
+      </ul>
+    </div>
+  );
+}
+export default Instruction;
