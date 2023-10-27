6c6c3c03ab636a865a173820507f8aa189e8df2a
diaasaur <diaasaur@gmail.com>
2023-10-27 10:53:09 +0530

    update hook name

--- src/hooks/useRecipeQuery.tsx
+++ src/hooks/useRecipeQuery.tsx
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import fetchRecipe from "../api/recipe";
 import { Recipe } from "../api/recipe.types";
 
-export default function useGetRecipe(id: string) {
+export default function useRecipeQuery(id: string) {
   return useQuery<Recipe, Error>({
     queryKey: ["recipe", id],
     queryFn: ({ signal }) => fetchRecipe(id, { signal }),
