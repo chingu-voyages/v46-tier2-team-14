9941aa799cc1031a84be890013051538331658cb
diaasaur <diaasaur@gmail.com>
2023-10-27 11:48:26 +0530

    add `MetaBadges` component
    quick badges showing: servings, link to recipe video and user ratings

---
+++ src/pages/recipe/recipe-meta/meta-badges/MetaBadges.tsx
@@ -0,0 +1,34 @@
+import { AiOutlineLike } from "react-icons/ai";
+import { BiBowlHot, BiLinkExternal } from "react-icons/bi";
+
+import Badge from "../../../../components/badge/Badge";
+import useRecipe from "../../../../hooks/useRecipe";
+import styles from "./meta-badges.module.css";
+
+export default function MetaBadges() {
+  const recipe = useRecipe();
+
+  return (
+    <div className={styles.badges}>
+      <Badge>
+        <BiBowlHot aria-hidden />
+        {recipe.num_servings}{" "}
+        {recipe.num_servings > 1
+          ? recipe.servings_noun_plural
+          : recipe.servings_noun_singular}
+      </Badge>
+      <Badge>
+        <AiOutlineLike aria-hidden />
+        {new Intl.NumberFormat("en-US", { style: "percent" }).format(
+          recipe.user_ratings.count_positive /
+            (recipe.user_ratings.count_positive +
+              recipe.user_ratings.count_negative),
+        )}{" "}
+        would make again
+      </Badge>
+      <Badge as="a" href={recipe.original_video_url} target="_blank">
+        <BiLinkExternal aria-hidden /> watch video
+      </Badge>
+    </div>
+  );
+}
