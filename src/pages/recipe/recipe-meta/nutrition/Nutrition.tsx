5ed472fab9dc5ddb13f364bae0b6e7e392869a73
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2023-10-28 18:16:20 +0530

    added the nutrition

--- src/pages/recipe/recipe-meta/nutrition/Nutrition.tsx
+++ src/pages/recipe/recipe-meta/nutrition/Nutrition.tsx
@@ -1,7 +1,35 @@
+import styles from "./nutrition.module.css";
+
 export default function Nutrition() {
   return (
-    <section>
+    <>
       <h2>Nutrition per serving</h2>
-    </section>
+      <section className={styles.container}>
+        <ul>
+          <span>Calories</span>
+          <h3>50</h3>
+        </ul>
+        <ul>
+          <span>Carbs</span>
+          <h3>1</h3>
+        </ul>
+        <ul>
+          <span>Fat</span>
+          <h3>4</h3>
+        </ul>
+        <ul>
+          <span>Protein</span>
+          <h3>7</h3>
+        </ul>
+        <ul>
+          <span>Sugar</span>
+          <h3>8</h3>
+        </ul>
+        <ul>
+          <span>Fiber</span>
+          <h3>6</h3>
+        </ul>
+      </section>
+    </>
   );
 }
