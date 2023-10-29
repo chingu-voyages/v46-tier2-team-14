ac4e47f1d3836aa913eb6d0bc7ce8799cb277722
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2023-10-29 13:43:57 +0530

    aaded the ingredient section

--- src/pages/recipe/recipe-ingredients/RecipeIngredients.tsx
+++ src/pages/recipe/recipe-ingredients/RecipeIngredients.tsx
@@ -1,3 +1,31 @@
+import useRecipe from "../../../hooks/useRecipe";
+import styles from "../recipe.module.css";
+
 export default function RecipeIngredients() {
-  return <div>RecipeIngredients</div>;
+  const recipe = useRecipe();
+  if (
+    !recipe.sections ||
+    !Array.isArray(recipe.sections) ||
+    recipe.sections.length === 0
+  ) {
+    return null;
+  }
+  return (
+    <section>
+      <h2>Ingredients</h2>
+      <div className={styles.instructions}>
+        <ul>
+          {recipe.sections.map((section) =>
+            section.components &&
+            Array.isArray(section.components) &&
+            section.components.length > 0
+              ? section.components.map((component) => (
+                  <li key={component.id}>{component.raw_text}</li>
+                ))
+              : null,
+          )}
+        </ul>
+      </div>
+    </section>
+  );
 }
