cadce70bc962166aa9be5fd5a577db3e8e3b45aa
diaasaur <diaasaur@gmail.com>
2023-10-29 17:45:56 +0530

    update dummy data ref

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,5 @@
 import RecipeList from "../../components/recipe-list/RecipeList";
-import data from "../../util/data.json";
+import recipes from "../../data/recipes.json";
 import Hero from "./hero/Hero";
 import styles from "./home.module.css";
 
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Hero />
-      <RecipeList recipes={data} query="Cheese" />
+      <RecipeList recipes={recipes} query="Cheese" />
       {/* TODO: component for under 30mins recipes here */}
     </main>
   );
