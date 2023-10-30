efa6fc6093ced7e5eea6080e9acc8cd56bb2d734
diaasaur <diaasaur@gmail.com>
2023-10-30 13:06:26 +0530

    add checks before displaying data

--- src/pages/recipe/recipe-meta/meta-badges/MetaBadges.tsx
+++ src/pages/recipe/recipe-meta/meta-badges/MetaBadges.tsx
@@ -10,25 +10,31 @@ export default function MetaBadges() {
 
   return (
     <div className={styles.badges}>
-      <Badge>
-        <BiBowlHot aria-hidden />
-        {recipe.num_servings}{" "}
-        {recipe.num_servings > 1
-          ? recipe.servings_noun_plural
-          : recipe.servings_noun_singular}
-      </Badge>
-      <Badge>
-        <AiOutlineLike aria-hidden />
-        {new Intl.NumberFormat("en-US", { style: "percent" }).format(
-          recipe.user_ratings.count_positive /
-            (recipe.user_ratings.count_positive +
-              recipe.user_ratings.count_negative),
-        )}{" "}
-        would make again
-      </Badge>
-      <Badge as="a" href={recipe.original_video_url} target="_blank">
-        <BiLinkExternal aria-hidden /> watch video
-      </Badge>
+      {recipe.num_servings && (
+        <Badge>
+          <BiBowlHot aria-hidden />
+          {recipe.num_servings}{" "}
+          {recipe.num_servings > 1
+            ? recipe.servings_noun_plural
+            : recipe.servings_noun_singular}
+        </Badge>
+      )}
+      {recipe.user_ratings && (
+        <Badge>
+          <AiOutlineLike aria-hidden />
+          {new Intl.NumberFormat("en-US", { style: "percent" }).format(
+            recipe.user_ratings.count_positive /
+              (recipe.user_ratings.count_positive +
+                recipe.user_ratings.count_negative),
+          )}{" "}
+          would make again
+        </Badge>
+      )}
+      {recipe.original_video_url && (
+        <Badge as="a" href={recipe.original_video_url} target="_blank">
+          <BiLinkExternal aria-hidden /> watch video
+        </Badge>
+      )}
     </div>
   );
 }
