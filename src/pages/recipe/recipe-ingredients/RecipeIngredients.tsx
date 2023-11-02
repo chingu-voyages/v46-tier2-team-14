1b54d0cbc22707104e059964828544a0966519a0
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2023-11-02 16:34:31 +0530

    feat: detailed recipe (#52)
    * hook to consume recipe info api
    
    * rename hook
    
    * add `useMedia` util hook
    
    tracks changes to provided media query
    
    * add `Badge` component
    
    * make border more visible
    
    * add styles to `Recipe`
    
    * add `MetaBadges` component
    
    quick badges showing: servings, link to recipe video and user ratings
    
    * add `TimeInfo` component
    
    shows prep, cook, total times for the recipe or total time tier depending on what is available
    
    * placeholder `Nutrition` component
    
    * update `RecipeMeta`
    
    * adding nutrition g
    
    * added the instruction
    
    * added css for nutrition
    
    * added basic style to instruction
    
    * added the nutrition
    
    * added nutritions block
    
    * added data from local api
    
    * added  media query
    
    * added media query
    
    * added instruction section
    
    * aaded the ingredient section
    
    * added css file of ingredients and instructions
    
    * change the class name
    
    * added the font weight
    
    * changes in Nutrition
    
    * deleted unused components
    
    * changes in background
    
    * add checks before displaying data
    
    * use a var for layout gap
    
    * update isBigScreen width
    
    * add sm screen text variant for likes
    
    * update badge
    
    * add font-size to nutri
    
    * increasing font-weight of nutri h3 for better visibility
    
    * cleanup
    
    * using dummy data to check netlify preview
    
    ---------
    
    Co-authored-by: diaasaur <diaasaur@gmail.com>

--- src/pages/recipe/recipe-ingredients/RecipeIngredients.tsx
+++ src/pages/recipe/recipe-ingredients/RecipeIngredients.tsx
@@ -1,3 +1,32 @@
+import useRecipe from "../../../hooks/useRecipe";
+import styles from "./recipe-ingredients.module.css";
+
 export default function RecipeIngredients() {
-  return <div>RecipeIngredients</div>;
+  const recipe = useRecipe();
+  if (
+    !recipe.sections ||
+    !Array.isArray(recipe.sections) ||
+    recipe.sections.length === 0
+  ) {
+    return null;
+  }
+  return (
+    <section>
+      <h2>Ingredients</h2>
+      <div className={styles.ingrediants}>
+        {recipe.sections.map((section) => (
+          <div key={section.position} className={styles.ingredientSection}>
+            <h2>{section.name}</h2>
+            <ul>
+              {section.components &&
+                Array.isArray(section.components) &&
+                section.components.map((component) => (
+                  <li key={component.id}>{component.raw_text}</li>
+                ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
---
+++ src/pages/recipe/recipe-meta/meta-badges/MetaBadges.tsx
@@ -0,0 +1,42 @@
+import { AiOutlineLike } from "react-icons/ai";
+import { BiBowlHot, BiLinkExternal } from "react-icons/bi";
+
+import Badge from "../../../../components/badge/Badge";
+import useMedia from "../../../../hooks/useMedia";
+import useRecipe from "../../../../hooks/useRecipe";
+import styles from "./meta-badges.module.css";
+
+export default function MetaBadges() {
+  const recipe = useRecipe();
+  const isMediumScreen = useMedia("(min-width: 768px");
+
+  return (
+    <div className={styles.badges}>
+      {recipe.num_servings && (
+        <Badge>
+          <BiBowlHot aria-hidden />
+          {recipe.num_servings}{" "}
+          {recipe.num_servings > 1
+            ? recipe.servings_noun_plural
+            : recipe.servings_noun_singular}
+        </Badge>
+      )}
+      {recipe.user_ratings && (
+        <Badge>
+          <AiOutlineLike aria-hidden />
+          {new Intl.NumberFormat("en-US", { style: "percent" }).format(
+            recipe.user_ratings.count_positive /
+              (recipe.user_ratings.count_positive +
+                recipe.user_ratings.count_negative),
+          )}{" "}
+          {isMediumScreen ? "would make again" : "likes"}
+        </Badge>
+      )}
+      {recipe.original_video_url && (
+        <Badge as="a" href={recipe.original_video_url} target="_blank">
+          <BiLinkExternal aria-hidden /> watch video
+        </Badge>
+      )}
+    </div>
+  );
+}
