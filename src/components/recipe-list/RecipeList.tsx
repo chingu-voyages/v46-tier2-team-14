16aa7fcd60ed59a7c38db3e2d7e51330e53265b0
Dikshant Agarwal <dikshantagarwal234@gmail.com>
2023-11-11 18:00:02 +0530

    Implemented infinite Loading

--- src/components/recipe-list/RecipeList.tsx
+++ src/components/recipe-list/RecipeList.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useRef, useState } from "react";
+import { BsArrowUpCircle } from "react-icons/bs";
 import { MdClear } from "react-icons/md";
 import { useNavigate, useParams } from "react-router-dom";
 
 import useSearchRecipes from "../../hooks/useSearchRecipe";
-// import { Recipe } from "../../types/recipe.types";
+import { Recipe } from "../../types/recipe.types";
 import styles from "./recipe-list.module.css";
 import RecipePreview from "./recipe-preview/RecipePreview";
 
@@ -12,20 +14,73 @@ import RecipePreview from "./recipe-preview/RecipePreview";
 // };
 
 // export default function RecipeList({ recipes, query }: Props) {
+const SHOW_RECIPE_PER_LOAD = 20;
+const SHOW_RECIPE_ON_INITIAL_LOAD = 20;
+const BUFFER_SCROLL_HEIGHT = 150;
 export default function RecipeList() {
   const { searchText } = useParams();
+  const [start, setStart] = useState<number>(0);
+  const [moveToTop, setMoveToTop] = useState<boolean>(false);
   const { data } = useSearchRecipes({
-    from: 0,
-    size: 20,
+    from: start,
+    size: SHOW_RECIPE_ON_INITIAL_LOAD,
     q: searchText,
     tags: searchText ? undefined : "under_30_minutes",
   });
-
+  const [recipeToShow, setRecipeToShow] = useState<Recipe[] | []>([]);
   const navigate = useNavigate();
+  const recipeListRef = useRef<HTMLUListElement>(null);
+  const recipePreviewRef = useRef<HTMLUListElement>(null);
 
   const handelClear = () => {
     navigate("/");
   };
+
+  const onLoadMore = () => {
+    const allRecipe = data && recipeToShow ? [...recipeToShow, ...data] : [];
+    setRecipeToShow(allRecipe);
+  };
+  const handleShowMoreRecipe = () => {
+    onLoadMore();
+    setMoveToTop(true);
+    setStart(start + SHOW_RECIPE_PER_LOAD);
+  };
+
+  const handleVisibleButton = () => {
+    const position = window.pageYOffset;
+    const headerElement = document.getElementById("header");
+    const heroElement = document.getElementById("hero");
+    const totalScrollHeight =
+      headerElement && heroElement && recipePreviewRef.current
+        ? headerElement.scrollHeight +
+          heroElement.scrollHeight +
+          recipePreviewRef.current.scrollHeight
+        : 0;
+    if (position > totalScrollHeight + BUFFER_SCROLL_HEIGHT) {
+      setMoveToTop(true);
+    } else if (position < totalScrollHeight + BUFFER_SCROLL_HEIGHT) {
+      setMoveToTop(false);
+    }
+  };
+  useEffect(() => {
+    if (data) {
+      setRecipeToShow(data);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleVisibleButton);
+    return () => window.removeEventListener("scroll", handleVisibleButton);
+  }, []);
+
+  const handleScrollToTop = () => {
+    recipeListRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
+    });
+  };
+
   return (
     <section className={styles.container}>
       {searchText ? (
@@ -43,16 +98,39 @@ export default function RecipeList() {
       ) : (
         <h2 className={styles.title}>👀 Recipes under 30 min </h2>
       )}
-      <ul className={styles.list}>
-        {data?.map(({ id, thumbnail_url, name }) => (
+      <ul className={styles.list} ref={recipeListRef}>
+        {recipeToShow?.map(({ id, thumbnail_url, name }) => (
           <RecipePreview
             key={id}
             id={id}
             name={name}
             thumbnail_url={thumbnail_url}
+            ref={recipePreviewRef}
           />
         ))}
       </ul>
+      {recipeToShow.length > 0 && (
+        <div className={styles.moveToLoad}>
+          <button
+            type="button"
+            onClick={handleShowMoreRecipe}
+            className={styles.moveToLoadButton}
+          >
+            Load More
+          </button>
+        </div>
+      )}
+      {moveToTop && (
+        <div className={styles.moveToTop}>
+          <button
+            type="button"
+            className={styles.moveToTopButton}
+            onClick={handleScrollToTop}
+          >
+            <BsArrowUpCircle size={28} />
+          </button>
+        </div>
+      )}
     </section>
   );
 }
--- src/components/recipe-list/recipe-preview/RecipePreview.tsx
+++ src/components/recipe-list/recipe-preview/RecipePreview.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-arrow-callback */
+import { forwardRef, Ref } from "react";
 import { BsArrowUpRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
@@ -6,11 +8,18 @@ import { Recipe } from "../../../types/recipe.types";
 import Skeleton from "../../skeleton/Skeleton";
 import styles from "./recipe-preview.module.css";
 
-export default function RecipePreview({ id, name, thumbnail_url }: Recipe) {
+const RecipePreview = forwardRef(function RecipePreview(
+  { id, name, thumbnail_url }: Recipe,
+  ref,
+) {
   const isImageLoaded = useImageLoaded(thumbnail_url);
 
   return (
-    <Link to={`recipe/${id}`} className={styles.recipe}>
+    <Link
+      to={`recipe/${id}`}
+      className={styles.recipe}
+      ref={ref as unknown as Ref<HTMLAnchorElement>}
+    >
       {isImageLoaded ? (
         <img
           src={thumbnail_url}
@@ -31,4 +40,6 @@ export default function RecipePreview({ id, name, thumbnail_url }: Recipe) {
       </div>
     </Link>
   );
-}
+});
+
+export default RecipePreview;
